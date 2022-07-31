766ab1a0665b9b4e3fb6bd146b2dc9e6664dface
Hamza Susam <hamza.susam@jetlink.io>
2022-07-31 18:33:37 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users:[],
+    isLoading: true
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(data => data.json()).then(users => {
+        // console.log(users);
+        this.setState({
+          users: users,
+          isLoading: false
+        });
+      })
+    }, 3000);
+    
+  }
+
+  render () {
+    const{isLoading} = this.state;
+    return (
+      <div className='App'>
+        <h1>Users</h1>
+        {isLoading ? 'Loading...' : ''}
+        {
+          this.state.users.map(user =>
+            <div key={user.id}>
+              {
+                user.name
+              }
+              &nbsp;-&nbsp;@
+              {
+                user.username
+              }
+            </div>
+            )
+        }
+      </div>
+    )
+  }
+  
 }
 
 export default App;
